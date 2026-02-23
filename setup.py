c8c90cd9c9d9338758acbcef48f19559ceb0cd07
KANISHKA P M <kanishkaraju99@gmail.com>
2026-02-23 19:52:39 +0530

    Add setup.py for package setup and installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ThyroDIAG',  # Replace with your package name
+    version='0.1.0',  # Replace with your package version
+    packages=find_packages(),
+    install_requires=[  # Include your project's dependencies here
+        # 'dependency1',
+        # 'dependency2',
+    ],
+    author='Kanishk PM',
+    author_email='your_email@example.com',  # Replace this with your email
+    description='A brief description of your package',  # Add a brief description
+    url='https://github.com/kanishkapm/ThyroDIAG',  # Replace with your project's URL
+)
